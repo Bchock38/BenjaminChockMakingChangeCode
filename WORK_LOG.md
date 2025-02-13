5605d218f39ee8d92a0021289e584aa1fea5aa94
Benjamin Chock <benjamin.chock@menloschool.org>
2025-02-13 10:19:47 -0800

    commit 5

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,13 +1,13 @@
 Name: Benjamin Chock
 
-| Date       |    Time     |                                                                                                                                                               Update |
-|:-----------|:-----------:|---------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
-| April 5    |   9-10pm    |                                                                                                                           I tried to write abstract, efficient code. |
-| January 27 |   9-9:55    |                                   Brainstormed different ways to solve making change. Tried to use modules to solve, but it didn't work to many outside way to solve |
-| January 28 |  2 - 2:30   |                                                                                                                         Started doing new attempt different solution |
-| Feb 1st    |    9-10     | Thought about how to approach new solutions contniued to code it. Consufused my self a bit on how to do smaller umber by number but think I've thought of a good way |
-| Feb 10     | 12:50 -1:20 |                                                                                                          finished my first solution for memoazation but doesn't work |
-| Feb 12     |   9- 9:12   |                          fixed the first solution for memoization. forget to add if target is 0 return 1, so it had nothing to add/build up from now passes all test |
+| Date      |    Time     |                                                                                                                                                                             Update |
+|:----------|:-----------:|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| January 27 |   9-9:55    |                                                 Brainstormed different ways to solve making change. Tried to use modules to solve, but it didn't work to many outside way to solve |
+| January 28 |  2 - 2:30   |                                                                                                                                       Started doing new attempt different solution |
+| Feb 1st   |    9-10     |               Thought about how to approach new solutions contniued to code it. Consufused my self a bit on how to do smaller umber by number but think I've thought of a good way |
+| Feb 10    | 12:50 -1:20 |                                                                                                                        finished my first solution for memoazation but doesn't work |
+| Feb 12    |   9- 9:30   | fixed the first solution for memoization. forget to add if target is 0 return 1, so it had nothing to add/build up from now passes all test. started making my tabulation solution |
+| Feb 13    |  10:-10:08  |                                             finished my first attempt at tab. forgot to get index of coins.length-1 instaed of coins.length. fixed that and then passed all 6 test |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MakingChange.java
+++ src/MakingChange.java
@@ -11,27 +11,42 @@ import java.util.Arrays;
 public class MakingChange {
     /**
      */
-    //do smallest number, then second smallest number + smallest but only one of them
-    //then do target minus 2nd smallest number and repeat till can't anyone
-    //move on to next biggest number
     static long[][] steps;
     public static long countWays(int target, int[] coins) {
       long ways = 0;
-      steps = new long[coins.length][target+1];
+      // Call Memoization solver
+      // ways = callMemo(target,coins);
+      // Call Tabulation solver
+       ways = countTab(coins,target);
+       // Return the number of coin solutions
+       return ways;
+    }
+
+    // Function that sets up and call Memoization solver
+    public static long callMemo(int target, int[] coins){
+        long ways = 0;
+        // Intialize array to hold number of steps for each used coin combo
+        steps = new long[coins.length][target+1];
+        // Fill array with -1 so I can check if it's empty or not while not getting it confused with actual 0s
         for (long[] step : steps) {
             Arrays.fill(step, -1);
         }
+        // Sort coin array from greatest to least
         Arrays.sort(coins);
+        // Run memoization solver
         ways = countMemo(target,coins,0);
-        return ways;
+        // Return all the coin combos you can use
+        return  ways;
     }
-
     public static long countMemo (int target, int[] coins, int start) {
         long ways = 0;
+        // If this coin combo has been used take the already known number of steps
         if (steps[start][target] != -1){
             return steps[start][target];
         }
+        // If target is zero steps will be equal to 1
         if (target == 0){
+            // Save the number of steps this took
             steps[start][target] = 1;
             return 1;
         }
@@ -39,19 +54,40 @@ public class MakingChange {
         if (target-coins[start] >= 0){
             ways = countMemo(target-coins[start],coins,start);
         }
-
-
         // Exclude
         if (start+1 < coins.length){
             ways += countMemo(target, coins, start+1);
         }
-
-
+        // Take the steps from the orginal target and all the coins
         steps[start][target] = ways;
-
+        // Return that value
         return ways;
-
     }
 
+    public static long countTab (int[] coins, int target){
+        // Make table for holding tabulation saved coin values
+        long[][] tabVals = new long[coins.length][target+1];
+        // Travese the whole 2d Array
+        for (int i = 0; i < tabVals.length; i++){
+            for (int j = 0; j < tabVals[i].length; j++){
+                // If target is 0 steps will always be 1
+                if (j == 0){
+                    tabVals[i][j] = 1;
+                }
+                else {
+                     // Add the value from the box above your current box if it's inbounds
+                     if (i -1 >= 0){
+                         tabVals[i][j] += tabVals[i-1][j];
+                     }
+                     // Add the value from the box the coin value to the left if it's in bounds
+                     if (j - coins[i] >= 0){
+                         tabVals[i][j] += tabVals[i][j- coins[i]];
+                     }
+                }
+            }
+        }
+        // Return the steps from the box that lines up with orginal target and all the coin values
+        return tabVals[coins.length-1][target];
+    }
 
 }
